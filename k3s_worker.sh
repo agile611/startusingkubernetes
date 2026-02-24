0dc30a6aaef8f29779c096c0737170373c9bc14d
Guillem Hernandez Sola <guillem@agile611.com>
2026-02-24 10:36:21 +0100

    Worker sh

--- k3s_worker.sh
+++ k3s_worker.sh
@@ -1,10 +1,13 @@
-  echo "⏳ Esperant que el Master generi el token..."
-  while [ ! -f /vagrant/node-token ]; do sleep 2; done
+#Anem a buscar el node token al Master
+sudo cat /var/lib/rancher/k3s/server/node-token
   
-  K3S_TOKEN=$(cat /vagrant/node-token)
-  NODE_IP=$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+echo "⏳ Esperant que el Master generi el token..."
+while [ ! -f /vagrant/node-token ]; do sleep 2; done
+  
+K3S_TOKEN=$(cat /vagrant/node-token)
+NODE_IP=$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
-  echo "🚀 Instal·lant K3s Agent (Worker) connectant a #{IP_MASTER}..."
-  curl -sfL https://get.k3s.io | K3S_URL=https://#{IP_MASTER}:6443 K3S_TOKEN=$K3S_TOKEN sh -s - --node-ip $NODE_IP --flannel-iface eth1
+echo "🚀 Instal·lant K3s Agent (Worker) connectant a #{IP_MASTER}..."
+curl -sfL https://get.k3s.io | K3S_URL=https://#{IP_MASTER}:6443 K3S_TOKEN=$K3S_TOKEN sh -s - --node-ip $NODE_IP --flannel-iface eth1
   
-  echo "✅ Worker unit al clúster!"
+echo "✅ Worker unit al clúster!"
